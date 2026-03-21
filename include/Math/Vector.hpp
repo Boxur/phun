80db41460187760d2ed97b09d17e3f83f489a9ab
Boxur <burekgamingpl@wp.pl>
2026-03-21 02:40:17 +0100

    Add 3D Vector class with common vector operations
    Introduced a Vector class in the phun namespace to represent 3D vectors with float components. Implemented standard vector operations such as addition, subtraction, dot and cross products, scalar multiplication/division, normalization, and component accessors. Included a non-member operator for scalar multiplication and used <cmath> for mathematical functions.

--- include/Math/Vector.hpp
+++ include/Math/Vector.hpp
@@ -1,1 +1,35 @@
 #pragma once
+#include <cmath>
+
+namespace phun
+{
+
+	class Vector
+	{
+	public:
+		Vector(float x, float y, float z);
+		Vector(Vector& other);
+
+		Vector operator+(const Vector& other)	const;
+		Vector operator-(const Vector& other)	const;
+		float operator*(const Vector& other)	const;
+		Vector operator*(const float& scalar)	const;
+		Vector operator/(const float& scalar)	const;
+		Vector operator^(const Vector& other)	const;
+
+		float Length()							const;
+
+		Vector Normal()							const;
+		void Normalize();
+
+		float& x();
+		float& y();
+		float& z();
+
+
+	private:
+		float x_, y_, z_;
+	};
+
+	Vector operator*(const float& scalar, const Vector& vector);
+}
--- src/Math/Vector.cpp
+++ src/Math/Vector.cpp
@@ -1,1 +1,64 @@
 #include "Vector.hpp"
+
+namespace phun
+{
+	Vector::Vector(float x, float y, float z) : x_(x), y_(y), z_(z) {}
+
+	Vector::Vector(Vector& other) : x_(other.x_), y_(other.y_), z_(other.z_) {}
+
+	Vector Vector::operator+(const Vector& other) const
+	{
+		return Vector(x_+other.x_,y_+other.y_,z_+other.z_);
+	}
+
+	Vector Vector::operator-(const Vector& other) const
+	{
+		return Vector(x_ - other.x_, y_ - other.y_, z_ - other.z_);
+	}
+
+	float Vector::operator*(const Vector& other) const
+	{
+		return x_*other.x_+ y_ * other.y_ + z_ * other.z_;
+	}
+
+	Vector Vector::operator*(const float& scalar) const
+	{
+		return Vector(x_*scalar,y_*scalar,z_*scalar);
+	}
+
+	Vector Vector::operator/(const float& scalar) const
+	{
+		return Vector(x_/scalar,y_/scalar,z_/scalar);
+	}
+
+	Vector Vector::operator^(const Vector& other) const
+	{
+		return Vector(y_ * other.z_ - z_ * other.y_, z_ * other.x_ - x_ * other.z_, x_ * other.y_ - y_ * other.x_);
+	}
+
+	float Vector::Length() const
+	{
+		return sqrtf(x_ * x_ + y_ * y_ + z_ * z_);
+	}
+	
+	Vector Vector::Normal() const
+	{
+		return (*this) / Length();
+	}
+
+	void Vector::Normalize()
+	{
+		*this = Normal();
+	}
+
+	float& Vector::x() { return x_; }
+	float& Vector::y() { return y_; }
+	float& Vector::z() { return z_; }
+
+	Vector operator*(const float& scalar, const Vector& vector)
+	{
+		return vector * scalar;
+	}
+
+}
+
