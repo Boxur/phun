93e2297b212609cf1495921e5de51b41c0a897f5
Boxur <burekgamingpl@wp.pl>
2026-03-26 21:57:41 +0100

    Refactor ObjectManager to use type-safe template handles
    Replaces Object<T> and std::any storage with a template-based ObjectManager<T> and ID<T> handle system. Objects are now managed in type-specific vectors, improving type safety and performance. Reference counting and dynamic casting are removed. All logic is now header-only; ObjectManager.cpp is obsolete.

--- Object/include/ObjectManager.hpp
+++ Object/include/ObjectManager.hpp
@@ -1,35 +1,92 @@
 #pragma once
-#include "ObjectBase.hpp"
-#include "Object.hpp"
+#include <memory>
 #include <mutex>
 #include <vector>
-#include <memory>
-#include <any>
 
 namespace phun
 {
+
+	template<class T>
+	class ID
+	{
+		template<class U>
+		friend class ObjectManager;
+	public:
+		ID() = delete;
+		~ID()
+		{
+			ObjectManager<T>::instance().UnregisterObject(*this);
+		}
+
+		T& operator*()
+		{
+			return ObjectManager<T>::instance().GetValue(*this);
+		}
+
+		size_t id() const
+		{
+			return id_;
+		}
+
+	private:
+
+		ID(const size_t& id) : id_(id) {}
+
+		size_t id_;
+	};
+
+	template<class T>
 	class ObjectManager
 	{
-		template<class T>
-		friend class Object;
 	public:
-		static ObjectManager& instance();
+		
+	public:
+		inline static ObjectManager& ObjectManager::instance()
+		{
+			static ObjectManager inst;
+			return inst;
+		}
+
+		ID<T> CreateObject()
+		{
+			std::lock_guard<std::mutex> lock(mutex_);
+			if (objects_.size() >= ids_.size())
+			{
+				objectIndexes_.push_back(objectIndexes_.size());
+				ids_.push_back(ids_.size());
+			}
+			objects_.push_back(T{});
+			return ids_[ids_.size() - 1];
+		}
+
+		void UnregisterObject(const ID<T>& id)
+		{
+			std::lock_guard<std::mutex> lock(mutex_);
+			if (objectIndexes_[id.id()] != objects_.size() - 1)
+			{
+				std::swap(ids_[objectIndexes_[id.id()]], ids_[objects_.size() - 1]);
+				std::swap(objects_[objectIndexes_[id.id()]], objects_[objects_.size() - 1]);
+				objectIndexes_[ids_[objectIndexes_[id.id()]]] = objectIndexes_[id.id()];
+				objectIndexes_[ids_[objects_.size() - 1]] = objects_.size() - 1;
+			}
+			objects_.pop_back();
+		}
+
+		T& GetValue(const ID<T>& id)
+		{
+			return objects_[objectIndexes_[id.id()]];
+		}
+
 	private:
 		ObjectManager() {}
 		ObjectManager(const ObjectManager&) = delete;
 		ObjectManager& operator=(const ObjectManager&) = delete;
 
-		void RegisterObject(ObjectBase* obj,std::any value);
-
-		void UnregisterObject(ObjectBase& obj);
-
-		std::any& GetValue(ObjectBase& obj);
-
 		std::mutex mutex_;
 
-		std::vector<size_t>		  objectIndexes_;
-		std::vector<size_t>		  ids_;
-		std::vector<ObjectBase*>  objects_;
-		std::vector<std::any>	  values_;
+		std::vector<size_t>	objectIndexes_;
+		std::vector<size_t> ids_;
+		std::vector<T>		objects_;
 	};
+
 }
