6863c76f1fd98ee8739ee488ed1e2e4701fbd1d6
Boxur <burekgamingpl@wp.pl>
2026-03-27 21:41:42 +0100

    Implement reference counting in ObjectManager and ID
    Added reference counting to ObjectManager and ID classes to manage object lifetimes. ID now supports copy construction and assignment, properly registering and unregistering references. Objects are deleted only when the last reference is destroyed. Added refCounts_ vector and RegisterObject/UnregisterObject methods. Improved thread safety and fixed object creation and deletion logic. GetValue now returns a default object for invalid IDs.

--- Object/include/ObjectManager.hpp
+++ Object/include/ObjectManager.hpp
@@ -1,7 +1,8 @@
 #pragma once
-#include <memory>
 #include <mutex>
 #include <vector>
+#include <cstddef>
+#include <utility>
 
 namespace phun
 {
@@ -13,6 +14,21 @@ namespace phun
 		friend class ObjectManager;
 	public:
 		ID() = delete;
+
+		ID(const ID<T>& other)
+		{
+			id_ = other.id_;
+			ObjectManager<T>::instance().RegisterObject(*this);
+		}
+
+		ID<T>& operator=(const ID<T>& other)
+		{
+			ObjectManager<T>::instance().UnregisterObject(*this);
+			id_ = other.id_;
+			ObjectManager<T>::instance().RegisterObject(*this);
+			return *this;
+		}
+
 		~ID()
 		{
 			ObjectManager<T>::instance().UnregisterObject(*this);
@@ -38,10 +54,10 @@ namespace phun
 	template<class T>
 	class ObjectManager
 	{
+		template<class U>
+		friend class ID;
 	public:
-		
-	public:
-		inline static ObjectManager& ObjectManager::instance()
+		inline static ObjectManager& instance()
 		{
 			static ObjectManager inst;
 			return inst;
@@ -53,28 +69,22 @@ namespace phun
 			if (objects_.size() >= ids_.size())
 			{
 				objectIndexes_.push_back(objectIndexes_.size());
-				ids_.push_back(ids_.size());
+				ids_.push_back(ids_.size()+1);
+				refCounts_.push_back(0);
 			}
 			objects_.push_back(T{});
+			++refCounts_[ids_.size() - 1];
 			return ids_[ids_.size() - 1];
-		}
-
-		void UnregisterObject(const ID<T>& id)
-		{
-			std::lock_guard<std::mutex> lock(mutex_);
-			if (objectIndexes_[id.id()] != objects_.size() - 1)
-			{
-				std::swap(ids_[objectIndexes_[id.id()]], ids_[objects_.size() - 1]);
-				std::swap(objects_[objectIndexes_[id.id()]], objects_[objects_.size() - 1]);
-				objectIndexes_[ids_[objectIndexes_[id.id()]]] = objectIndexes_[id.id()];
-				objectIndexes_[ids_[objects_.size() - 1]] = objects_.size() - 1;
-			}
-			objects_.pop_back();
 		}
 
 		T& GetValue(const ID<T>& id)
 		{
-			return objects_[objectIndexes_[id.id()]];
+			static T zero;
+			zero = {};
+			if (id.id_ == 0)
+				return zero;
+			std::lock_guard<std::mutex> lock(mutex_);
+			return objects_[objectIndexes_[id.id_-1]];
 		}
 
 	private:
@@ -82,10 +92,37 @@ namespace phun
 		ObjectManager(const ObjectManager&) = delete;
 		ObjectManager& operator=(const ObjectManager&) = delete;
 
+		void RegisterObject(const ID<T>& id)
+		{
+			std::lock_guard<std::mutex> lock(mutex_);
+			++refCounts_[objectIndexes_[id.id_ - 1]];
+		}
+
+		void UnregisterObject(ID<T>& id)
+		{
+			if (id.id_ == 0)
+				return;
+			int index = id.id_ - 1;
+			std::lock_guard<std::mutex> lock(mutex_);
+			if (--refCounts_[index] != 0)
+				return;
+			if (objectIndexes_[index] != objects_.size() - 1)
+			{
+				std::swap(ids_[objectIndexes_[index]], ids_[objects_.size() - 1]);
+				std::swap(objects_[objectIndexes_[index]], objects_[objects_.size() - 1]);
+				std::swap(refCounts_[objectIndexes_[index]], refCounts_[objects_.size() - 1]);
+				objectIndexes_[ids_[objectIndexes_[index]]] = objectIndexes_[index];
+				objectIndexes_[ids_[objects_.size() - 1]] = objects_.size() - 1;
+			}
+			objects_.pop_back();
+			id.id_ = 0;
+		}
+
 		std::mutex mutex_;
 
 		std::vector<size_t>	objectIndexes_;
 		std::vector<size_t> ids_;
+		std::vector<size_t> refCounts_;
 		std::vector<T>		objects_;
 	};
 
